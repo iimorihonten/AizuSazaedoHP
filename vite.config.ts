4915e0e9c0e9bb3f7628328e107501276968c254
Rentan55 <changethewind2015@gmail.com>
2025-01-25 11:18:33 +0900

    fix: カスタムドメイン対応のためのパス設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/AizuSazaedoHP/',
+  base: process.env.CUSTOM_DOMAIN ? '/' : '/AizuSazaedoHP/',
   server: {
     port: 5180
   },
